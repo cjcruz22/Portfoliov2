b931d39efdd130f2b67b1d354e35d55f27b4f7d4
cjcruz22 <143507178+cjcruz22@users.noreply.github.com>
2023-11-06 21:12:21 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+var typed= new Typed (".text",{
+strings: [ " Gamer " ," Rider ", " Web Developer  "],
+typeSpeed:50,
+backSpeed:50,
+backDelay:1000  , 
+loop:true
+});
+var typed= new Typed (".corp",{
+    strings: [ " You Name IT Corp."],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000  , 
+    loop:true
+    });
+    
